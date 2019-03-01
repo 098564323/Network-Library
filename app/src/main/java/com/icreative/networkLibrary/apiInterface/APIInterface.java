c2d3504f3d6362cb250c3c5145b3c4c39e820466
098564323 <43173426+098564323@users.noreply.github.com>
2019-03-01 16:03:40 +0530

    Delete APIInterface.java

--- app/src/main/java/com/icreative/networkLibrary/apiInterface/APIInterface.java
+++
@@ -1,7 +0,0 @@
-package com.icreative.networkLibrary.apiInterface;
-
-import io.reactivex.Observable;
-
-public interface APIInterface {
-    <T> void getResponseData(Observable<T> observableData);
-}
